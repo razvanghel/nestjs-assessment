5827c618d397e57666b8a2dcd6dcd23b70d9a29f
AnghelRazvan <aanghelrazvan99@gmail.com>
2026-03-24 19:49:44 +0100

    add global exception handler

--- backend/src/cocktails/cocktails.service.ts
+++ backend/src/cocktails/cocktails.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, InternalServerErrorException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { Cocktails } from './cocktails.entity';
