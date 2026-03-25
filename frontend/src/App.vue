29dea338b8c9b29ccac6550012e93335b21df836
AnghelRazvan <aanghelrazvan99@gmail.com>
2026-03-25 21:54:51 +0100

    reformat(frontend): remove nav links

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,12 +1,6 @@
 <template>
   <div id="app">
     <Toaster position="top-right" richColors closeButton expand />
-    <nav>
-      <ul>
-        <li><router-link to="/">List</router-link></li>
-        <li><router-link to="/new">new cocktail</router-link></li>
-      </ul>
-    </nav>
     <router-view />
   </div>
 </template>
