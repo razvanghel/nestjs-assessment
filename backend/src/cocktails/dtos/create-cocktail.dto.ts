7ec0b3ea1b648d291b4862b981f6948bf2ddf86d
AnghelRazvan <aanghelrazvan99@gmail.com>
2026-03-25 21:01:59 +0100

    feat(backend): add update cocktail dto

--- backend/src/cocktails/dtos/create-cocktail.dto.ts
+++ backend/src/cocktails/dtos/create-cocktail.dto.ts
@@ -2,14 +2,14 @@ import { IsString, IsNumber, Min, Length } from 'class-validator';
 
 export class CreateCocktailDto {
   @IsString()
-  @Length(2, 100)
+  @Length(2, 80)
   title: string;
 
   @IsString()
-  @Length(5, 500)
+  @Length(5, 200)
   description: string;
 
   @IsNumber()
-  @Min(0)
+  @Min(0.01)
   price: number;
 }
